004b38b41b6402dfe462011f26655751ebcec250
Angel Leon <elonlegan@gmail.com>
2020-08-17 18:44:33 -0500

    all working nice yet, arquitecture basic for add games ready

--- script/games.js
+++ script/games.js
@@ -1,3 +1,21 @@
+let games = [
+  { name: "arkanoid", runGame: false },
+  { name: "donkeyKong", runGame: false },
+  { name: "pong", runGame: false },
+  { name: "pacman", runGame: false },
+  { name: "frogger", runGame: false },
+  { name: "spaceInvaders", runGame: false },
+  { name: "tetris", runGame: false },
+  { name: "Galaga", runGame: false },
+  { name: "centipede", runGame: false },
+];
+
 function changeGame(gameChoose) {
-  console.log(`si funciona si muestra el ${gameChoose}`);
+  for (let index = 0; index < games.length; index++) {
+    games[index].runGame = false;
+  }
+  games[gameChoose].runGame = true;
+  console.log(
+    `si funciona, si muestra el ${games[gameChoose].name} y si esta corriendo ${games[gameChoose].runGame}`
+  );
 }
