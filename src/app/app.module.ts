fd41f6ab7813a855f049528157457935a4e52438
Angel Leon <angel@leanware.io>
2023-05-12 12:23:21 -0500

    divided in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsComponent } from './components/news/news.component';
+import { DividerComponent } from './components/divider/divider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    RulesComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewsComponent,
+    DividerComponent
   ],
   imports: [
     BrowserModule
